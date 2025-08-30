dc92e33a2ad02650d1b69a2c9d4e5c7b1147fc7b
Robert Hagen <rdh@stealthllama.org>
2025-08-30 09:15:52 -0400

    Added export functionality

--- publisher.py
+++ publisher.py
@@ -31,6 +31,25 @@ def update_pub(instance, session, pub_id, pub_dict):
         response.raise_for_status()
 
 
+def export_pub(instance, session, pub_id, pub_dict):
+    # Format the payload
+    payload = {
+        'format': 'Pdf',
+        'outputFileName': "Storage/" + pub_id + '.pdf',
+        'exportPresetName': 'Default'
+    }
+    # Send the API call
+    response = session.post(
+        url = "https://" + instance + "/api/v1/projects/" + pub_dict['project'] + "?action=export",
+        json = payload
+    )
+    if response.ok:
+        task_key = response.json()['taskKey']
+        return(task_key)
+    else:
+        response.raise_for_status()
+
+
 def wait_for_success(session, instance, task):
     success_status = False
     # Keep checking until the task is successful
@@ -70,7 +89,7 @@ def main():
     with open(PUBFILE, 'r') as f:
         pub_dict = yaml.safe_load(f)
 
-    # Process each of the projects
+    # Publish each of the publications
     pub_list = list(pub_dict.keys())
     for pub in pub_list:
         task_id = update_pub(
@@ -80,9 +99,23 @@ def main():
             pub_dict[pub]
         )
         # Check the status of the publishing task
-        print(f"Updating {pub} publication via task {task_id}")
+        print(f"Publishing {pub} via task {task_id}")
         timer = wait_for_success(session, INSTANCE, task_id)
         print(f"Publishing task {task_id} completed after {timer} seconds")
+
+    # Export each of the publications
+    for exp in pub_list:
+        if pub_dict[exp]['export'] is True:
+            task_id = export_pub(
+                INSTANCE,
+                session,
+                exp,
+                pub_dict[exp]
+            )
+            # Check the status of the export task
+            print(f"Exporting {exp} via task {task_id}")
+            timer = wait_for_success(session, INSTANCE, task_id)
+            print(f"Export task {task_id} completed after {timer} seconds")
 
 
 if __name__ == "__main__":
